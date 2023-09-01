a03560d3348e1e6a8be0b3885a1b28e394bb9db1
agustinantonucci <agustinantonucci@gmail.com>
2023-09-01 15:28:12 -0300

    fix: URL

--- src/config/apolloClientConfig.js
+++ src/config/apolloClientConfig.js
@@ -1,24 +1,22 @@
 /* eslint-disable no-unused-vars */
-import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
-
+import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
 const PORT = `4002`; //puerto de escucha de GraphQL
 const PROTOCOL = window.location.protocol;
 const HOSTNAME = window.location.hostname;
 const SEARCH = window.location.search;
-const URL = `${PROTOCOL}//${HOSTNAME}:${PORT}/${SEARCH}`;
-
+const URL = `${PROTOCOL}//${HOSTNAME}:${PORT}/graphql`;
 
 const Client = new ApolloClient({
-	connectToDevTools: true,
-	cache: new InMemoryCache(),
-	link: new HttpLink({
-		uri: `http://10.0.0.153:4002/graphql`
-		//uri: process.env.REACT_APP_APOLLO,
-		//uri: URL,
-		// uri: `http://beeapp.binamics.com.ar:4002`
-		// fetch,
-	}),
+  connectToDevTools: true,
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    // uri: `http://10.0.0.153:4002/graphql`
+    //uri: process.env.REACT_APP_APOLLO,
+    uri: URL,
+    // uri: `http://beeapp.binamics.com.ar:4002`
+    // fetch,
+  }),
 });
 
 export default Client;
